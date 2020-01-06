4e824944c700ece4bc365b8f7e893f45d7269879
hazy <gracrys@gmail.com>
2020-01-05 23:43:44 -0430

    initial commit

---
+++ src/components/Tasks.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let tasks = []
+</script>
+
+<ul>
+    {#if tasks.lenght < 1}
+        <li>No pending tasks</li>
+    {:else}
+        {#each tasks as task, task.id}
+            <li>{task}</li>
+        {/each}
+    {/if}
+
+</ul>
+
